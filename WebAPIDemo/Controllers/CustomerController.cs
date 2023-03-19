d73d230d29bc1f0fed817165058cf06e0446e856
thinhnd125 <thinhnd125@gmail.com>
2023-03-20 02:06:06 +0700

    Web API Help Page

---
+++ WebAPIDemo/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["webapi_conn"].ConnectionString);
+        Customer cus = new Customer();
+        // GET api/values
+        /// <summary>
+        /// Lấy toàn bộ thông tin khach hang
+        /// </summary>
+        /// <returns></returns>
+        public void GetKH()
+        {
+
+        }
+    }
+}
--- WebAPIDemo/Controllers/ValuesController.cs
+++ WebAPIDemo/Controllers/ValuesController.cs
@@ -17,9 +17,14 @@ namespace WebAPIDemo.Controllers
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["webapi_conn"].ConnectionString);
         Employee emp = new Employee();
         // GET api/values
+        /// <summary>
+        /// Lấy toàn bộ thông tin nhân viên
+        /// </summary>
+        /// <returns></returns>
         public List<Employee> Get()
         {
             SqlDataAdapter adapter = new SqlDataAdapter("AA_GetEmployee", con);
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
             DataTable dt = new DataTable();
             adapter.Fill(dt);
             List<Employee> listEmployee = new List<Employee>();
@@ -28,31 +33,62 @@ namespace WebAPIDemo.Controllers
                 for (int i = 0; i< dt.Rows.Count; i++)
                 {
                     Employee emp = new Employee();
-                    emp.Id = dt.Rows[i]["Id"].ToString();
-                    emp.Name = dt.Rows[i]["Name"].ToString();
-                    emp.Address = dt.Rows[i]["Address"].ToString();
-                    emp.Center = dt.Rows[i]["Center"].ToString();
-                    emp.Type = Convert.ToInt16(dt.Rows[i]["Type"]);
+                    emp.Id = dt.Rows[i]["idEmployee"].ToString();
+                    emp.Name = dt.Rows[i]["nameEmployee"].ToString();
+                    emp.Address = dt.Rows[i]["address"].ToString();
+                    emp.Center = dt.Rows[i]["center"].ToString();
+                    emp.Type = Convert.ToInt16(dt.Rows[i]["otc"]);
                     listEmployee.Add(emp);
                 }
-                if (listEmployee.Count > 0)
-                {
-                    return listEmployee;
-                }
-                else 
-                { 
-                    return null;
-                }
+            }
+            if (listEmployee.Count > 0)
+            {
+                return listEmployee;
+            }
+            else
+            {
+                return null;
             }
         }
 
         // GET api/values/5
-        public string Get(int id)
+        /// <summary>
+        /// Lấy thông tin nhân viên theo ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Employee Get(string id)
         {
-            return "value";
+            SqlDataAdapter adapter = new SqlDataAdapter("AA_GetEmployeeById", con);
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            adapter.SelectCommand.Parameters.AddWithValue("@Id", id);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            Employee emp = new Employee();
+            if (dt.Rows.Count > 0)
+            {
+                emp.Id = dt.Rows[0]["idEmployee"].ToString();
+                emp.Name = dt.Rows[0]["nameEmployee"].ToString();
+                emp.Address = dt.Rows[0]["address"].ToString();
+                emp.Center = dt.Rows[0]["center"].ToString();
+                emp.Type = Convert.ToInt16(dt.Rows[0]["otc"]);
+            }
+            if (emp != null)
+            {
+                return emp;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         // POST api/values
+        /// <summary>
+        /// Thêm nhân viên mới
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
         public string Post(Employee employee)
         {
             String msg = "";
@@ -83,13 +119,67 @@ namespace WebAPIDemo.Controllers
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
+        /// <summary>
+        /// Cập nhật thông tin nhân viên
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public string Put(string id, Employee employee)
         {
+            String msg = "";
+            if (employee != null)
+            {
+                SqlCommand cmd = new SqlCommand("AA_UpdateEmployeeById", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Address", employee.Address);
+                cmd.Parameters.AddWithValue("@Center", employee.Center);
+                cmd.Parameters.AddWithValue("@Type", employee.Type);
+
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+
+                if (i > 0)
+                {
+                    msg = "Data is updated";
+                }
+                else
+                {
+                    msg = "Error";
+                }
+            }
+            return msg;
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        /// <summary>
+        /// Xóa nhân viên
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string Delete(string id)
         {
+            String msg = "";
+            SqlCommand cmd = new SqlCommand("AA_DeleteEmployeeById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i > 0)
+            {
+                msg = "Data is deleted";
+            }
+            else
+            {
+                msg = "Error";
+            }
+            return msg;
         }
     }
 }
